0fd0dbda8839ae8d59cc0c855c91d3f9f1bc869f
Denys <dodenisa@gmail.com>
2021-02-21 14:18:54 +0200

    lab01 - 8 (text editor completed)

--- lab01/MainWindow.xaml.cs
+++ lab01/MainWindow.xaml.cs
@@ -15,14 +15,60 @@ using System.Windows.Shapes;
 
 namespace lab01
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        void CanExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (inputTextBox.Text.Trim().Length > 0)
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+        }
+
+        void Execute_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            System.IO.File.WriteAllText(@".\myFile.txt", inputTextBox.Text);
+            MessageBox.Show("The file was saved!");
+        }
+
+        void CanExecute_Open(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (System.IO.File.ReadAllText(@".\myFile.txt").Trim().Length > 0)
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+        }
+
+        void Execute_Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            inputTextBox.Text = System.IO.File.ReadAllText(@".\myFile.txt");
+        }
+
+        void CanExecute_Delete(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (inputTextBox.SelectedText.Trim().Length > 0)
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+        }
+
+        void Execute_Delete(object sender, ExecutedRoutedEventArgs e)
+        {
+            inputTextBox.SelectedText = "";
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            CommandBinding saveCommand = new CommandBinding(ApplicationCommands.Save, Execute_Save, CanExecute_Save);
+            CommandBinding openCommand = new CommandBinding(ApplicationCommands.Open, Execute_Open, CanExecute_Open);
+            CommandBinding deleteCommand = new CommandBinding(ApplicationCommands.Delete, Execute_Delete, CanExecute_Delete);
+
+            CommandBindings.Add(saveCommand);
+            CommandBindings.Add(openCommand);
+            CommandBindings.Add(deleteCommand);
+
         }
     }
 }
